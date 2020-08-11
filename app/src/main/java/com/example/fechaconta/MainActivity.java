3ff9f85d263316daf27b0bdde98e0eb268410238
Leonardo Patrocínio <lpatrocinioreis@gmail.com>
2020-08-11 19:38:40 -0300

    V0.0.1 - Classes + Conexao Firebase

--- app/src/main/java/com/example/fechaconta/MainActivity.java
+++ app/src/main/java/com/example/fechaconta/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.fechaconta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Teste";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
