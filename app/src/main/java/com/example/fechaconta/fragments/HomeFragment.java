0f72a20ed6f7024dc7a8213f207b6c359d779ba6
hjnum <GiovannyoGodoi@gmail.com>
2020-08-12 19:58:13 -0300

    V0.0.5 - Adapters e Recycler

--- app/src/main/java/com/example/fechaconta/fragments/HomeFragment.java
+++ app/src/main/java/com/example/fechaconta/fragments/HomeFragment.java
@@ -14,7 +14,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fechaconta.R;
 import com.example.fechaconta.adapter.CategoryAdapter;
+import com.example.fechaconta.adapter.RestaurantAdapter;
 import com.example.fechaconta.models.Category;
+import com.example.fechaconta.models.Restaurant;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -28,6 +30,7 @@ public class HomeFragment extends Fragment {
     private final String TAG = "HOMEFRAGMENT";
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private RecyclerView recyclerViewCategory;
+    private RecyclerView recyclerViewRestaurant;
 
 
    public HomeFragment(){
@@ -37,32 +40,59 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_home, container,false);
+       View view =inflater.inflate(R.layout.fragment_home, container,false);
+
         recyclerViewCategory = view.findViewById(R.id.recycler_categorias);
+        recyclerViewRestaurant = view.findViewById(R.id.recycler_restaurantes);
 
-        RecyclerView.LayoutManager layManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
-
-        recyclerViewCategory.setLayoutManager(layManager);
+        //Instancia os layout Manager e configura os recycler
+        RecyclerView.LayoutManager layManagerCat = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView.LayoutManager layManagerRes = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerViewCategory.setLayoutManager(layManagerCat);
         recyclerViewCategory.setHasFixedSize(true);
         recyclerViewCategory.isNestedScrollingEnabled();
+        recyclerViewRestaurant.setLayoutManager(layManagerRes);
+        recyclerViewRestaurant.setHasFixedSize(true);
+        recyclerViewRestaurant.isNestedScrollingEnabled();
+
+        //Recupera os Restaurantes Ordenados pela media do Firestore
+        db.collection("Restaurant").orderBy("media").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    List<Restaurant> listRes = new ArrayList<>();
+                    for(DocumentSnapshot document : task.getResult()){
+                        listRes.add(document.toObject(Restaurant.class));
+                        Log.d(TAG, "onComplete:  =====> " + document.getData() );
+
+                    }
+
+                    RestaurantAdapter restaurantAdapter = new RestaurantAdapter(listRes);
+                    recyclerViewRestaurant.setAdapter(restaurantAdapter);
+
+                }else{
+                    Log.d(TAG, "onFailure: Falha ao recuperar os Restaurantes");
+                }
+
+
+            }
+        });
 
 
 
-
-
-
-
-
+        //Recupera as Categorias do Firestore
         db.collection("Category").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()){
                     List<Category> listCat = new ArrayList<>();
                     for (DocumentSnapshot document : task.getResult()){
+                        //Joga as Categorias no List
                         listCat.add(document.toObject(Category.class));
                         Log.d(TAG, "onComplete: ======>" + document.getData().toString());
                     }
 
+                    //Instancia e seta o Adapter com a lista de categorias
                     CategoryAdapter categoryAdapter = new CategoryAdapter(listCat, listCat.size());
                     recyclerViewCategory.setAdapter(categoryAdapter);
 
