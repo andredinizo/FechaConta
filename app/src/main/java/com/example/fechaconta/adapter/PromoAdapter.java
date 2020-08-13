c892e35a90456bf684937e917a35e19a5e0a18c8
hjnum <GiovannyoGodoi@gmail.com>
2020-08-12 23:05:13 -0300

    V0.0.6 - Promoções e SrollView

--- app/src/main/java/com/example/fechaconta/adapter/PromoAdapter.java
+++ app/src/main/java/com/example/fechaconta/adapter/PromoAdapter.java
@@ -1,34 +1,66 @@
 package com.example.fechaconta.adapter;
 
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-class PromoAdapter extends RecyclerView.Adapter<PromoAdapter.MyViewHolder>{
+import com.example.fechaconta.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromoAdapter extends RecyclerView.Adapter<PromoAdapter.MyViewHolder>{
+
+    private int listSize;
 
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_promo, parent, false);
+        return new MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        final StorageReference imagem = storage.getReference().child("Categorias/"+ holder.listUrl.get(position));
+        imagem.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).resize(360*2,150*2).centerCrop().into(holder.imageView);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 5;
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder{
+        List<String> listUrl = new ArrayList<>();
+        ImageView imageView;
+
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.imagempromo);
+            listUrl.add("lanchecategoria.jpeg");
+            listUrl.add("pizza.jpg");
+            listUrl.add("arabe.jpg");
+            listUrl.add("japonesa.jpg");
+            listUrl.add("brasileira.jpg");
+
         }
     }
 
--- app/src/main/java/com/example/fechaconta/fragments/HomeFragment.java
+++ app/src/main/java/com/example/fechaconta/fragments/HomeFragment.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fechaconta.R;
 import com.example.fechaconta.adapter.CategoryAdapter;
+import com.example.fechaconta.adapter.PromoAdapter;
 import com.example.fechaconta.adapter.RestaurantAdapter;
 import com.example.fechaconta.models.Category;
 import com.example.fechaconta.models.Restaurant;
@@ -21,16 +22,20 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import ru.tinkoff.scrollingpagerindicator.ScrollingPagerIndicator;
 
 public class HomeFragment extends Fragment {
     private final String TAG = "HOMEFRAGMENT";
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private RecyclerView recyclerViewCategory;
     private RecyclerView recyclerViewRestaurant;
+    private RecyclerView recyclerViewPromo;
 
 
    public HomeFragment(){
@@ -42,21 +47,34 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        View view =inflater.inflate(R.layout.fragment_home, container,false);
 
+       PromoAdapter promoAdapter = new PromoAdapter();
+
         recyclerViewCategory = view.findViewById(R.id.recycler_categorias);
         recyclerViewRestaurant = view.findViewById(R.id.recycler_restaurantes);
+        recyclerViewPromo = view.findViewById(R.id.recyclerpromo);
 
         //Instancia os layout Manager e configura os recycler
         RecyclerView.LayoutManager layManagerCat = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         RecyclerView.LayoutManager layManagerRes = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        RecyclerView.LayoutManager layManagerPromo = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+
         recyclerViewCategory.setLayoutManager(layManagerCat);
         recyclerViewCategory.setHasFixedSize(true);
-        recyclerViewCategory.isNestedScrollingEnabled();
+        recyclerViewCategory.setNestedScrollingEnabled(true);
         recyclerViewRestaurant.setLayoutManager(layManagerRes);
         recyclerViewRestaurant.setHasFixedSize(false);
-        recyclerViewRestaurant.isNestedScrollingEnabled();
+        recyclerViewRestaurant.setNestedScrollingEnabled(false);
+        recyclerViewPromo.setLayoutManager(layManagerPromo);
+        recyclerViewPromo.setAdapter(promoAdapter);
+
+        ScrollingPagerIndicator recyclerIndicator = view.findViewById(R.id.dotspromocao);
+        recyclerIndicator.attachToRecyclerView(recyclerViewPromo);
+
+
 
         //Recupera os Restaurantes Ordenados pela media do Firestore
-        db.collection("Restaurant").orderBy("media").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        Query queryRes = db.collection("Restaurant").orderBy("media", Query.Direction.DESCENDING);
+        queryRes.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()){
