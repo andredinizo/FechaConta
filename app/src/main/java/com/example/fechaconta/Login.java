a1a9d088c2cf2ebb9c951a8990a1f1383745979b
André <andredinizo@gmail.com>
2020-09-07 16:32:05 -0300

    Login com email e botão facebook inacabado

--- app/src/main/java/com/example/fechaconta/Login.java
+++ app/src/main/java/com/example/fechaconta/Login.java
@@ -1,6 +1,7 @@
 package com.example.fechaconta;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -8,6 +9,7 @@ import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -28,6 +30,12 @@ public class Login extends AppCompatActivity {
     public TextView logo;
     private FirebaseAuth mAuth;
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        verifyAuth();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,7 +90,6 @@ public class Login extends AppCompatActivity {
         public void run() {
             // logo.setVisibility(View.GONE);
             //getSupportFragmentManager().beginTransaction().add(R.id.FragContainer, new LoginFragment()).commit();
-            verifyAuth();
         }
     };
 
@@ -101,8 +108,9 @@ public class Login extends AppCompatActivity {
     }
 
     public void verifyAuth () {
-            FirebaseUser currentUser = mAuth.getCurrentUser();
-            if(currentUser != null){
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Log.d("LOGIN", "verifyAuth - current user = "+ currentUser);
+        if(currentUser != null){
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 this.finish();
@@ -111,10 +119,17 @@ public class Login extends AppCompatActivity {
             }
     }
 
+
+
+    /*
+    //onActivityResult do fb
     @Override
-    protected void onStart() {
-        super.onStart();
-
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            fragment.onActivityResult(requestCode, resultCode, data);
+        }
     }
+    */
+
 }
--- app/src/main/java/com/example/fechaconta/fragments/LoginFragment.java
+++ app/src/main/java/com/example/fechaconta/fragments/LoginFragment.java
@@ -17,29 +17,43 @@ import com.example.fechaconta.ActivityRegistro;
 import com.example.fechaconta.Login;
 import com.example.fechaconta.MainActivity;
 import com.example.fechaconta.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Arrays;
 
 public class LoginFragment extends Fragment {
     private final String TAG = "LOGIN_FRAGMENT";
     private static final int RC_SIGN_IN = 9001;
+    private LoginButton botao_facebook;
+    private CallbackManager fb_callback_manager;
     private Button botao_google;
-    private Button botao_facebook;
     private Button botao_login_email;
     private Button botao_desenvolvedor;
     private Button botao_criar_conta;
     private FirebaseAuth mAuth;
     private GoogleSignInClient googleSignInClient;
+    private boolean login_google;
 
     @Nullable
     @Override
@@ -73,6 +87,14 @@ public class LoginFragment extends Fragment {
 
 
 
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(getContext(), gso);
+
+
         botao_google = view.findViewById(R.id.botao_google);
         botao_google.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,15 +104,38 @@ public class LoginFragment extends Fragment {
         });
 
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        googleSignInClient = GoogleSignIn.getClient(getContext(), gso);
+
+
+
+        //preparação login facebook
+        fb_callback_manager = CallbackManager.Factory.create();
+        botao_facebook =view.findViewById(R.id.botao_facebook);
+        botao_facebook.setFragment(this);
+        botao_facebook.setPermissions("email","public_profile");
+        // fb callback registration
+        botao_facebook.registerCallback(fb_callback_manager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                firebaseAuthWithFacebook(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.d(TAG, "facebook:onError");
+            }
+        });
 
 
         return view;
     }
+
+
 
     //fução de entrar na main activity
     public void entrar() {
@@ -110,6 +155,7 @@ public class LoginFragment extends Fragment {
 
     private void signIn() {
         Intent signInIntent = googleSignInClient.getSignInIntent();
+        login_google=true;
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
@@ -117,10 +163,17 @@ public class LoginFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == RC_SIGN_IN){
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+        //google
+        if (login_google) {
+            if (requestCode == RC_SIGN_IN) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                handleSignInResult(task);
+            }
+            return;
         }
+
+        //facebook
+        fb_callback_manager.onActivityResult(requestCode, resultCode, data);
     }
 
     private void handleSignInResult(Task<GoogleSignInAccount> completeTask) {
@@ -128,10 +181,8 @@ public class LoginFragment extends Fragment {
             GoogleSignInAccount account = completeTask.getResult(ApiException.class);
             Log.d(TAG, "handleSignInResult: "+ account.getId());
             firebaseAuthWithGoogle (account.getIdToken());
-            entrar();
         } catch (ApiException e) {
             Log.w(TAG, "handleSignInResult:failed code=  "+ e.getStatusCode() );
-
         }
     }
 
@@ -153,4 +204,32 @@ public class LoginFragment extends Fragment {
                     }
                 });
     }
+
+
+
+    private void firebaseAuthWithFacebook(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            entrar();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getContext(), "Autentication Failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
